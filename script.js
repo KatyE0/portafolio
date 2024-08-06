a9e513a9e9cd5652517258725e07d136da2413d7
KaryEO <okaty9303@gmail.com>
2024-08-05 19:23:18 -0600

    actualización

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ typewriter
     .typeString('Desarrolladora WEB JUNIOR')
     .pauseFor(1000)
     .start();
+
+let frase = document.getElementById('frase');
+
+let typewriter2 = new Typewriter(frase, {
+    loop: true,
+    delay: 75,
+});
+
+typewriter2
+    .pauseFor(2500) // milisegundos 2.5 segundos
+    .typeString('"Cada línea de código es una oportunidad para innovar y mejorar."')
+    .pauseFor(300)
+    .deleteAll()
+    .typeString('....Por lo tanto, lo que debemos hacer es esforzarnos, perseverar y NUNCA RENDIRNOS')
+    .pauseFor(1000)
+    .start();
